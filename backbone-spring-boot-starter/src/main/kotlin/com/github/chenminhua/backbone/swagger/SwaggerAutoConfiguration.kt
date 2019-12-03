56ef5c22a3cb378da8c5678ab82ee00b3398e59e
chenminhua <chenmh@blacklake.cn>
2019-12-03 20:34:36 +0800

    change Gradle

--- backbone-spring-boot-starter/src/main/kotlin/com/github/chenminhua/backbone/swagger/SwaggerAutoConfiguration.kt
+++ backbone-spring-boot-starter/src/main/kotlin/com/github/chenminhua/backbone/swagger/SwaggerAutoConfiguration.kt
@@ -1,8 +1,10 @@
 package com.github.chenminhua.backbone.swagger
 
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@EnableSwagger2
 open class SwaggerAutoConfiguration {
 
 }
